aea9f9e6759854b3c0b0e275fa89e1cde159b37a
Yee <yee.gamedev@gmail.com>
2022-04-15 18:28:04 -0500

    Ship Rotates When in Corner

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -6,24 +6,23 @@ public class EnemyMover : MonoBehaviour
 {
     [SerializeField] [Range(0f, 10f)] float speed = 1f;
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
+    [SerializeField] Animator shipAnimator = null;
 
-    private void Start()
-    {
-        StartCoroutine(FollowPath());
-    }
+    private void Start() { StartCoroutine(FollowPath()); }
 
     private IEnumerator FollowPath()
     {
+        int i = 0;
         foreach (Waypoint waypoint in path)
         {
+            i++;
             Vector3 startPosition = transform.position;
             Vector3 endPosition = waypoint.transform.position;
             endPosition.y = transform.position.y;
+
+            RotateShip(waypoint);
+
             float travelPercent = 0f;
-            Quaternion quaternion = Quaternion.LookRotation(waypoint.transform.position - transform.position, Vector3.up);
-            Vector3 euler = Vector3.zero;
-            euler.y = quaternion.eulerAngles.y;
-            transform.rotation = Quaternion.Euler(euler);
 
             while (travelPercent < 1f)
             {
@@ -32,6 +31,28 @@ public class EnemyMover : MonoBehaviour
 
                 yield return new WaitForEndOfFrame();
             }
+
+            if (waypoint.GetIsCorner) { yield return RotateShip(path[i]); }
+        }
+    }
+
+    private IEnumerator RotateShip(Waypoint waypoint)
+    {
+        Quaternion startRotation = transform.rotation;
+        Vector3 lookTarget = waypoint.transform.position - transform.position;
+        lookTarget.y = 0;
+        Quaternion lookDirection = Quaternion.LookRotation(lookTarget, Vector3.up);
+
+        if (Vector3.Dot(transform.right, lookTarget) < 0) { shipAnimator.SetTrigger("TurningLeft"); }
+        else { shipAnimator.SetTrigger("TurningRight"); }
+
+        float travelPercent = 0f;
+        while (travelPercent < 1f)
+        {
+            travelPercent += Time.deltaTime * speed;
+            transform.rotation = Quaternion.Lerp(startRotation, lookDirection, travelPercent);
+
+            yield return new WaitForEndOfFrame();
         }
     }
 }
--- Assets/Tile/Waypoint.cs
+++ Assets/Tile/Waypoint.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Waypoint : MonoBehaviour
 {
     [SerializeField] bool canPlaceTower = false;
+    [SerializeField] bool isCorner = false;
+
+    public bool GetIsCorner { get => isCorner; }
 
     private void OnMouseDown()
     {
