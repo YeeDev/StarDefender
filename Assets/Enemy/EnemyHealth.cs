b16bbb4cd7d66c8f58589506bbd98f3391a45272
Yee <yee.gamedev@gmail.com>
2022-04-16 12:00:28 -0500

    Enemy Takes Damage

---
+++ Assets/Enemy/EnemyHealth.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] int maxHitPoints = 10;
+
+    int currentHitPoints = 0;
+
+    private void Awake() { currentHitPoints = maxHitPoints; }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        currentHitPoints--;
+        if (currentHitPoints <= 0) { Destroy(gameObject); }
+    }
+}
