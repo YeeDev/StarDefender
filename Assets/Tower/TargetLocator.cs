0be2480f6751c1fdfbdac5da9628a856284202c4
Yee <yee.gamedev@gmail.com>
2022-04-16 14:23:11 -0500

    Turrets Now Look at the Closest Target

--- Assets/Tower/TargetLocator.cs
+++ Assets/Tower/TargetLocator.cs
@@ -4,11 +4,42 @@ using UnityEngine;
 
 public class TargetLocator : MonoBehaviour
 {
+    [SerializeField] float attackRange = 5f;
     [SerializeField] Transform target = null;
     [SerializeField] Transform towerHead = null;
+    [SerializeField] LayerMask enemyLayer = 0;
+
+    Transform mainTarget = null;
 
     private void Update()
     {
-        towerHead.LookAt(target);
+        LocateClosestTarget();
+        towerHead.LookAt(mainTarget);
+    }
+
+    private void LocateClosestTarget()
+    {
+        Collider[] enemiesInRange = Physics.OverlapSphere(transform.position, attackRange, enemyLayer);
+        Transform closestTarget = null;
+        float maxDistance = Mathf.Infinity;
+
+        foreach (Collider enemy in enemiesInRange)
+        {
+            float enemyDistance = (enemy.transform.position - transform.position).sqrMagnitude;
+
+            if (enemyDistance < maxDistance)
+            {
+                closestTarget = enemy.transform;
+                maxDistance = enemyDistance;
+            }
+        }
+
+        mainTarget = closestTarget;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
     }
 }
