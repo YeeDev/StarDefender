aaab9e75ad10f5af71b4b46fcdc96f0fd4623b5a
Yee <yee.gamedev@gmail.com>
2022-04-27 09:06:07 -0500

    Space Station Can Take Damage

---
+++ Assets/Scripts/StationHealth.cs
@@ -0,0 +1,26 @@
+using System;
+using UnityEngine;
+
+public class StationHealth : MonoBehaviour
+{
+    public event Action OnTakeDamage;
+
+    [SerializeField] [Range(0, 100)] int maximumHealthPoints = 10;
+
+    public int GetHealthPoints { get => maximumHealthPoints; }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("EnemyMissile"))
+        {
+            TakeDamage(other.gameObject);
+        }
+    }
+
+    private void TakeDamage(GameObject missile)
+    {
+        Destroy(missile);
+        maximumHealthPoints--;
+        if (OnTakeDamage != null) { OnTakeDamage(); }
+    }
+}
