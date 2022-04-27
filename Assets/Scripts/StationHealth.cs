a696f337911ffbe45b8a01c0a2030880855ade9a
Yee <yee.gamedev@gmail.com>
2022-04-27 09:41:34 -0500

    Station Explodes When Taking Damage

--- Assets/Scripts/StationHealth.cs
+++ Assets/Scripts/StationHealth.cs
@@ -6,6 +6,7 @@ public class StationHealth : MonoBehaviour
     public event Action OnTakeDamage;
 
     [SerializeField] [Range(0, 100)] int maximumHealthPoints = 10;
+    [SerializeField] ParticleSystem explosion = null;
 
     public int GetHealthPoints { get => maximumHealthPoints; }
 
@@ -21,6 +22,7 @@ public class StationHealth : MonoBehaviour
     {
         Destroy(missile);
         maximumHealthPoints--;
+        explosion.Play();
         if (OnTakeDamage != null) { OnTakeDamage(); }
     }
 }
