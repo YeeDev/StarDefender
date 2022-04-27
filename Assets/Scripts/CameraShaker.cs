75e732933bbaea68b38b364f03af139965d8a9e5
Yee <yee.gamedev@gmail.com>
2022-04-27 09:24:45 -0500

    Camera Shakes When Station Takes Damage

---
+++ Assets/Scripts/CameraShaker.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using UnityEngine;
+
+public class CameraShaker : MonoBehaviour
+{
+	[SerializeField] [Range(1, 10)] float shakeDuration = 1f;
+	[SerializeField] [Range(0.1f, 5f)] float shakeIntensity = 1f;
+
+	Vector3 originalPos;
+	StationHealth stationHealth;
+
+    private void Awake()
+    {
+		originalPos = transform.position;
+		stationHealth = FindObjectOfType<StationHealth>();	
+	}
+
+	private void OnEnable() { stationHealth.OnTakeDamage += StartShake; }
+	private void OnDisable() { stationHealth.OnTakeDamage -= StartShake; }
+
+    private void StartShake() { StartCoroutine(Shake()); }
+
+	private IEnumerator Shake()
+	{
+		float shakeTimer = shakeDuration;
+		while (shakeTimer > 0)
+		{
+			shakeTimer -= Time.deltaTime;
+			transform.position = originalPos + Random.insideUnitSphere * shakeIntensity;
+
+			yield return new WaitForEndOfFrame();
+		}
+
+		transform.position = originalPos;
+	}
+}
