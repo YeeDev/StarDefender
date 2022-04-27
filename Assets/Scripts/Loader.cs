8079af1acb3b0ab97919dad71fd6b85eb3229eca
Yee <yee.gamedev@gmail.com>
2022-04-27 13:53:08 -0500

    Loader Script Created

---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loader : MonoBehaviour
+{
+    int currentLevel;
+    StationHealth stationHealth;
+
+    private void Awake()
+    {
+        stationHealth = FindObjectOfType<StationHealth>();
+        currentLevel = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    private void OnEnable() { stationHealth.OnTakeDamage += ReloadLevel; }
+    private void OnDisable() { stationHealth.OnTakeDamage -= ReloadLevel; }
+
+    private void ReloadLevel() { if (stationHealth.GetHealthPoints <= 0) { LoadLevel(currentLevel); } }
+
+    private void LoadLevel(int levelToLoad) { SceneManager.LoadScene(levelToLoad); }
+}
