b8cdb1ffaa8c730743e5edd71f32f470542981fe
Yee <yee.gamedev@gmail.com>
2022-04-30 09:47:40 -0500

    Dialogue Printer Imported and Fixed

---
+++ Assets/Scripts/DialoguePrinter.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialoguePrinter : MonoBehaviour
+{
+    bool skip;
+    bool inEndCode;
+    string endCode = "";
+
+    private IEnumerator PrintDialogue(string textToPrint, Text text)
+    {
+        string s = "";
+        endCode = "";
+        inEndCode = false;
+
+        for (int i = 0; i < textToPrint.Length; i++)
+        {
+            char c = textToPrint[i];
+            s += c;
+
+            if (skip)
+            {
+                if (c == '>')
+                {
+                    if (!inEndCode && endCode == "")
+                    {
+                        SearchForEndCode(textToPrint.Substring(i));
+                    }
+                    skip = false;
+                }
+
+                continue;
+            }
+
+            if (c == '<')
+            {
+                skip = true;
+
+                if (textToPrint[i + 1] == '/')
+                {
+                    endCode = "";
+                    inEndCode = true;
+                    continue;
+                }
+
+                inEndCode = false;
+                continue;
+            }
+
+            text.text = s + endCode;
+
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        Debug.Log("Finish printing");
+    }
+
+    private void SearchForEndCode(string text)
+    {
+        int startIndex = text.IndexOf('/') - 1;
+
+        if (startIndex < 0) { return; }
+
+        string checkSubstring = text.Substring(startIndex);
+        int endIndex = checkSubstring.IndexOf('>');
+
+        endCode += checkSubstring.Substring(0, endIndex + 1);
+
+        if (checkSubstring.Length >= (endIndex + 2))
+        {
+            if (checkSubstring[endIndex + 2] != '/') { return; }
+
+            SearchForEndCode(checkSubstring.Substring(endIndex + 1));
+        }
+    }
+}
