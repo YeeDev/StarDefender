08ca1ee072b061bbd4b0ca954b8937d0a3fe1c7c
Yee <yee.gamedev@gmail.com>
2022-09-05 12:13:37 -0500

    Ship Behaviour Script Finished (Refactor Needed)

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -2,27 +2,78 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class Ship : MonoBehaviour
 {
-    [SerializeField] float speed = 0.1f;
-    [SerializeField] float rotationSpeed = 1f;
+    [SerializeField] float speed = 2f;
+    [SerializeField] float rotationSpeed = 3f;
+    [Header("Debug, remove later")]
     [SerializeField] Transform[] pathArray = null;
 
-    int coordinateToMove = 0;
-    float f;
-    
-    void Update()
-    {
-        Vector3 positionWithHeight = pathArray[coordinateToMove].position;
-        positionWithHeight.y = transform.position.y;
-        transform.position = Vector3.MoveTowards(transform.position, positionWithHeight, speed);
+    int objectToMoveTo = 0;
+    bool rotating;
+    Animator anm;
 
-        if ((transform.position - positionWithHeight).sqrMagnitude <= 0)
-        {
-            coordinateToMove++;
-            f = 0;
-        }
+    private void Awake() { anm = GetComponent<Animator>(); }
+
+    private void Start()
+    {
+        Vector3 positionWithHeight = pathArray[0].position;
+        positionWithHeight.y = transform.position.y;
 
         transform.LookAt(positionWithHeight, Vector3.up);
+    }
+
+    private void Update() { MoveShip(); }
+
+    private void MoveShip()
+    {
+        Vector3 positionWithHeight = pathArray[objectToMoveTo].position;
+        positionWithHeight.y = transform.position.y;
+        transform.position = Vector3.MoveTowards(transform.position, positionWithHeight, speed * Time.deltaTime);
+
+        RotateShip(positionWithHeight);
+        SetNextTilePosition(positionWithHeight);
+    }
+
+    private void RotateShip(Vector3 positionWithHeight)
+    {
+        if (pathArray[objectToMoveTo].CompareTag("Corner")) { RotateSmoothly(); }
+        else { transform.LookAt(positionWithHeight, Vector3.up); }
+    }
+
+    private void RotateSmoothly()
+    {
+        Vector3 normalizedTilePosition = pathArray[objectToMoveTo + 1].position - transform.position;
+        normalizedTilePosition.y = transform.position.y;
+
+        Quaternion rot = Quaternion.LookRotation(normalizedTilePosition);
+
+        if (!rotating) { StartCoroutine(AnimateTurn(normalizedTilePosition)); }
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * rotationSpeed);
+    }
+
+    private IEnumerator AnimateTurn(Vector3 normalizedTilePosition)
+    {
+        rotating = true;
+
+        if (Vector3.Dot(transform.right, normalizedTilePosition) < 0) { anm.SetTrigger("TurningLeft"); }
+        else { anm.SetTrigger("TurningRight"); }
+
+        yield return new WaitForSeconds(1.1f);
+
+        rotating = false;
+    }
+
+    //TODO Grab it from another place, not here
+    private void SetNextTilePosition(Vector3 positionWithHeight)
+    {
+        if ((transform.position - positionWithHeight).sqrMagnitude <= 0)
+        {
+            if (objectToMoveTo + 1 >= pathArray.Length) { return; }
+
+            objectToMoveTo++;
+        }
     }
 }
