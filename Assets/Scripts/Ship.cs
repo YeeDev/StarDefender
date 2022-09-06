0f686a15bcacabf6d0b61b8d8fdfcca8aa8e24f7
Yee <yee.gamedev@gmail.com>
2022-09-06 18:10:51 -0500

    Ship now fires a missile while on goal

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -2,27 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//TODO Add to a namespace
+//TODO Ships need to die
 [RequireComponent(typeof(Animator))]
 public class Ship : MonoBehaviour
 {
     [SerializeField] float speed = 2f;
     [SerializeField] float rotationSpeed = 3f;
+    [SerializeField] GameObject missilePrefab = null;
+    [SerializeField] Transform hardpoint = null;
     [Header("Debug, remove later")]
     [SerializeField] Transform[] pathArray = null;
 
     int objectToMoveTo = 0;
     bool rotating;
+    bool missileFired;
     Animator anm;
 
     private void Awake() { anm = GetComponent<Animator>(); }
-
-    private void Start()
-    {
-        Vector3 positionWithHeight = pathArray[0].position;
-        positionWithHeight.y = transform.position.y;
-
-        transform.LookAt(positionWithHeight, Vector3.up);
-    }
 
     private void Update() { MoveShip(); }
 
@@ -32,11 +29,13 @@ public class Ship : MonoBehaviour
         positionWithHeight.y = transform.position.y;
         transform.position = Vector3.MoveTowards(transform.position, positionWithHeight, speed * Time.deltaTime);
 
-        RotateShip(positionWithHeight);
-        SetNextTilePosition(positionWithHeight);
+        CheckRotationType(positionWithHeight);
+        SetNextTilePosition(positionWithHeight); //TODO this shouldn't be here
+
+        if (objectToMoveTo + 1 == pathArray.Length && !missileFired) { Shoot(); }
     }
 
-    private void RotateShip(Vector3 positionWithHeight)
+    private void CheckRotationType(Vector3 positionWithHeight)
     {
         if (pathArray[objectToMoveTo].CompareTag("Corner")) { RotateSmoothly(); }
         else { transform.LookAt(positionWithHeight, Vector3.up); }
@@ -64,6 +63,12 @@ public class Ship : MonoBehaviour
         yield return new WaitForSeconds(1.1f);
 
         rotating = false;
+    }
+
+    private void Shoot()
+    {
+        missileFired = true;
+        Instantiate(missilePrefab, hardpoint.position, hardpoint.rotation);
     }
 
     //TODO Grab it from another place, not here
