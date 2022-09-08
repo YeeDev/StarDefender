52d94956477116102e5229a7d324269be1e0e599
Yee <yee.gamedev@gmail.com>
2022-09-08 12:29:39 -0500

    Path Finder finds any path (needs to be tests)

--- Assets/Scripts/PathFinder.cs
+++ Assets/Scripts/PathFinder.cs
@@ -1,89 +1,97 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using StarDef.Tiles;
 
-//TODO Namespace and create and actual path in awake, then let the ship grab it
-public class PathFinder : MonoBehaviour
+namespace StarDef.Paths
 {
-    [Tooltip("Unity's Grid Snap Settings")]
-    [SerializeField] int unityGridSize = 1;
-    [SerializeField] Vector2Int startCoordinate, endCoordinate;
-
-    Vector2Int[] directions = { Vector2Int.right, Vector2Int.up, Vector2Int.down, Vector2Int.left };
-
-    Queue<Tile> tilesToExplore = new Queue<Tile>();
-    List<Transform> pathTiles = new List<Transform>();
-    Dictionary<Vector2Int, Tile> grid = new Dictionary<Vector2Int, Tile>();
-
-    public List<Transform> GetPath { get => pathTiles; } 
-
-    private void Awake()
+    public class PathFinder : MonoBehaviour
     {
-        LoadTiles();
-        FindPath();
-    }
+        Vector2Int[] directions = { Vector2Int.right, Vector2Int.up, Vector2Int.down, Vector2Int.left };
 
-    private void LoadTiles()
-    {
-        var waypoints = FindObjectsOfType<Tile>();
-        foreach (Tile tile in waypoints)
+        Queue<Tile> tilesToExplore = new Queue<Tile>();
+        Dictionary<Vector2Int, Tile> grid = new Dictionary<Vector2Int, Tile>();
+
+        private void Awake() { LoadTiles(); }
+
+        private void LoadTiles()
         {
-            var gridPos = tile.GridCoordinates;
-            if (grid.ContainsKey(gridPos)) { Debug.LogWarning("Skipping overlapping block " + tile); }
-            else { grid.Add(gridPos, tile); }
-        }
-    }
-
-    private void FindPath()
-    {
-        tilesToExplore.Enqueue(grid[startCoordinate]);
-
-        while (tilesToExplore.Count > 0)
-        {
-            Tile exploringTile = tilesToExplore.Dequeue();
-
-            ExploreNeighbours(exploringTile);
-
-            if (exploringTile.GridCoordinates == endCoordinate)
+            var waypoints = FindObjectsOfType<Tile>();
+            foreach (Tile tile in waypoints)
             {
-                tilesToExplore.Clear();
+                var gridPos = tile.GridCoordinates;
+                if (grid.ContainsKey(gridPos)) { Debug.LogWarning("Skipping overlapping block " + tile); }
+                else { grid.Add(gridPos, tile); }
             }
         }
 
-        CreatePath();
-    }
-
-    private void ExploreNeighbours(Tile exploredTile)
-    {
-        exploredTile.AlreadyExplored = true;
-
-        foreach (Vector2Int direction in directions)
+        public List<Transform> FindPath(Vector2Int startCoordinate, Vector2Int endCoordinate)
         {
-            Vector2Int positionToSearch = exploredTile.GridCoordinates + direction;
-            if (grid.ContainsKey(positionToSearch))
+            ClearFlow();
+
+            tilesToExplore.Enqueue(grid[startCoordinate]);
+
+            while (tilesToExplore.Count > 0)
             {
-                Tile neighbour = grid[positionToSearch];
+                Tile exploringTile = tilesToExplore.Dequeue();
 
-                if (!neighbour.AlreadyExplored && neighbour.IsPath)
+                ExploreNeighbours(exploringTile);
+
+                if (exploringTile.GridCoordinates == endCoordinate)
                 {
-                    neighbour.AlreadyExplored = true;
-                    neighbour.TileConnectedTo = exploredTile;
+                    tilesToExplore.Clear();
+                }
+            }
 
-                    tilesToExplore.Enqueue(neighbour);
+            return CreatePath(endCoordinate);
+        }
+
+        private void ClearFlow()
+        {
+            foreach (var tile in grid)
+            {
+                grid[tile.Key].TileConnectedTo = null;
+                grid[tile.Key].AlreadyExplored = false;
+            }
+
+            tilesToExplore.Clear();
+        }
+
+        private void ExploreNeighbours(Tile exploredTile)
+        {
+            exploredTile.AlreadyExplored = true;
+
+            foreach (Vector2Int direction in directions)
+            {
+                Vector2Int positionToSearch = exploredTile.GridCoordinates + direction;
+                if (grid.ContainsKey(positionToSearch))
+                {
+                    Tile neighbour = grid[positionToSearch];
+
+                    if (!neighbour.AlreadyExplored && neighbour.IsPath)
+                    {
+                        neighbour.AlreadyExplored = true;
+                        neighbour.TileConnectedTo = exploredTile;
+
+                        tilesToExplore.Enqueue(neighbour);
+                    }
                 }
             }
         }
-    }
 
-    private void CreatePath()
-    {
-        Tile tileAddedToPath = grid[endCoordinate];
-        while (tileAddedToPath != null)
+        private List<Transform> CreatePath(Vector2Int endCoordinate)
         {
-            pathTiles.Add(tileAddedToPath.transform);
-            tileAddedToPath = tileAddedToPath.TileConnectedTo;
-        }
+            List<Transform> path = new List<Transform>();
 
-        pathTiles.Reverse();
+            Tile tileAddedToPath = grid[endCoordinate];
+            while (tileAddedToPath != null)
+            {
+                path.Add(tileAddedToPath.transform);
+                tileAddedToPath = tileAddedToPath.TileConnectedTo;
+            }
+
+            path.Reverse();
+
+            return path;
+        }
     }
 }
