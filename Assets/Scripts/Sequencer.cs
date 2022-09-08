31d58b0b210066c60804e1645c2a0337047abb76
Yee <yee.gamedev@gmail.com>
2022-09-08 12:30:34 -0500

    Sequence script plays wave sequences

---
+++ Assets/Scripts/Sequencer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sequencer : MonoBehaviour
+{
+    [SerializeField] WaveSO[] waves;
+
+    private void Start()
+    {
+        StartCoroutine(RunWaves());
+    }
+
+    private IEnumerator RunWaves()
+    {
+        foreach (WaveSO wave in waves)
+        {
+            yield return wave.PlayWave();
+        }
+    }
+}
