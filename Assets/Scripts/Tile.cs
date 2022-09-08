145ae20c90a0a250ce105ba561506670450b1645
Yee <yee.gamedev@gmail.com>
2022-09-08 00:16:00 -0500

    Tile script now has more functionality to work with a path finding system.

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -7,8 +7,25 @@ public class Tile : MonoBehaviour
 {
     [Tooltip("Unity's Grid Snap Settings")]
     [SerializeField] int unityGridSize = 2;
+    [SerializeField] bool isPath = false;
 
+    bool alreadyExplored;
     TextMesh text;
+
+    public bool IsPath { get => isPath; }
+    public bool AlreadyExplored { get => alreadyExplored; set => alreadyExplored = value; }
+    public Vector2Int GridCoordinates
+    {
+        get
+        {
+            Vector2Int coordinates = new Vector2Int();
+            coordinates.x = Mathf.RoundToInt(transform.position.x / unityGridSize);
+            coordinates.y = Mathf.RoundToInt(transform.position.z / unityGridSize);
+
+            return coordinates;
+        }
+    }
+
 
     private void Awake()
     {
@@ -18,10 +35,9 @@ public class Tile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2Int coordinates = new Vector2Int();
-        coordinates.x = Mathf.RoundToInt(transform.position.x / unityGridSize);
-        coordinates.y = Mathf.RoundToInt(transform.position.z / unityGridSize);
+        Vector2Int coordinates = GridCoordinates;
 
         text.text = $"({coordinates.x},{coordinates.y})";
+        transform.name = coordinates.ToString();
     }
 }
