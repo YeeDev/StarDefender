dfe67b560058f9cf5dee916bdec4310394750c54
Yee <yee.gamedev@gmail.com>
2022-09-08 12:30:17 -0500

    WaveSO script created

---
+++ Assets/Scripts/WaveSO.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StarDef.Paths;
+using StarDef.Enemies;
+
+[CreateAssetMenu(fileName = "New Wave", menuName = "Wave")]
+public class WaveSO : ScriptableObject
+{
+    [SerializeField] int numberOfEnemies = 5;
+    [SerializeField] float timeBetweenEnemies = 1f;
+    [SerializeField] GameObject enemyType = null;
+    [SerializeField] Vector2Int startPoint, endPoint;
+
+    public IEnumerator PlayWave()
+    {
+        int totalEnemiesSpawned = 0;
+        PathFinder pathFinder = FindObjectOfType<PathFinder>();
+        List<Transform> path = pathFinder.FindPath(startPoint, endPoint);
+
+        while (totalEnemiesSpawned < numberOfEnemies)
+        {
+            totalEnemiesSpawned++;
+            Instantiate(enemyType).GetComponent<Ship>().SetPath = path;
+            yield return new WaitForSeconds(timeBetweenEnemies);
+        }
+    }
+}
