5c9c26304ac53e0e2d91fcc3ea81081d58335b30
Yee <yee.gamedev@gmail.com>
2022-09-11 18:01:45 -0500

    Health Stat Script Created

---
+++ Assets/Scripts/HealthStat.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class HealthStat : MonoBehaviour
+{
+    Queue<EnergyGenerator> generators = new Queue<EnergyGenerator>();
+
+    public bool NoHealth { get => generators.Count <= 0; }
+
+    private void Awake()
+    {
+        foreach (var generator in FindObjectsOfType<EnergyGenerator>())
+        {
+            generators.Enqueue(generator);
+        }
+    }
+
+    public void TakeDamage()
+    {
+        if (generators.Count <= 0) { return; }
+
+        EnergyGenerator generator = generators.Dequeue();
+        generator.DamageGenerator();
+
+        if (generators.Count <= 0) { Debug.Log("You lost!"); }
+    }
+}
