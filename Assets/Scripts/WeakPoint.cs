5e2e708d665aa5da1d2263a062027b8c2f6ca0ae
Yee <yee.gamedev@gmail.com>
2022-09-11 18:01:54 -0500

    WeakPoint Script Created

---
+++ Assets/Scripts/WeakPoint.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using StarDef.Tiles;
+
+//TODO change name of script
+public class WeakPoint : MonoBehaviour
+{
+    HealthStat health;
+
+    private void Awake() { health = FindObjectOfType<HealthStat>(); }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Missile"))
+        {
+            Destroy(other.gameObject);
+            health.TakeDamage();
+        }
+    }
+}
