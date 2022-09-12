1dfc60ce118f1673ab28fa62a494579d47b9942b
Yee <yee.gamedev@gmail.com>
2022-09-12 16:16:59 -0500

    Weakpoint Namespace Changed

---
+++ Assets/Scripts/Interactables/WeakPoint.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace StarDef.Interactables
+{
+    public class WeakPoint : MonoBehaviour
+    {
+        HealthStat health;
+
+        private void Awake() { health = FindObjectOfType<HealthStat>(); }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Missile"))
+            {
+                Destroy(other.gameObject);
+                health.TakeDamage();
+            }
+        }
+    }
+}
