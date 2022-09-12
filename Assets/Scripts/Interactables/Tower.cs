be0dfc9e5687485a8ed6c54cc40dc430e411e511
Yee <yee.gamedev@gmail.com>
2022-09-12 16:19:14 -0500

    Tower Script now uses Energy Generators

---
+++ Assets/Scripts/Interactables/Tower.cs
@@ -0,0 +1,95 @@
+using System.Collections.Generic;
+using UnityEngine;
+using StarDef.Paths;
+using StarDef.Tiles;
+
+namespace StarDef.Interactables
+{
+    public class Tower : MonoBehaviour
+    {
+        [SerializeField] float attackRange = 1f;
+        [SerializeField] LayerMask enemyLayer = 0;
+        [SerializeField] Transform towerHead = null;
+        [SerializeField] Color enabledColor, disabledColor;
+
+        Animator animator;
+        EnergyFinder energyFinder;
+        EnergyGenerator generator;
+        List<MeshRenderer> indicators;
+
+        Transform mainTarget;
+
+        private void Awake()
+        {
+            animator = GetComponent<Animator>();
+            energyFinder = FindObjectOfType<EnergyFinder>();
+        }
+
+        private void Start()
+        {
+            indicators = energyFinder.FindEnergy(GetComponent<Tile>().GridCoordinates);
+            generator = indicators[0].GetComponentInParent<EnergyGenerator>();
+            generator.AddPath(indicators);
+        }
+
+        private void OnMouseDown()
+        {
+            if (!generator.IsOn || !generator.CanBeUsed(transform)) { return; }
+
+            animator.SetBool("IsOpen", !animator.GetBool("IsOpen"));
+            generator.UsedBy = animator.GetBool("IsOpen") ? transform : null;
+
+            ChangeIndicatorsColor();
+        }
+
+        private void ChangeIndicatorsColor()
+        {
+            Color colorToUse = animator.GetBool("IsOpen") ? enabledColor : disabledColor;
+
+            foreach (MeshRenderer indicator in indicators)
+            {
+                indicator.material.color = colorToUse;
+            }
+        }
+
+        private void Update()
+        {
+            LookAtClosestTarget();
+            ShootTarget();
+        }
+
+        private void LookAtClosestTarget()
+        {
+            if (!generator.IsOn) { return; }
+
+            Collider[] enemiesInRange = Physics.OverlapSphere(transform.position, attackRange, enemyLayer);
+            Transform closestTarget = null;
+            float maxDistance = Mathf.Infinity;
+
+            foreach (Collider enemy in enemiesInRange)
+            {
+                float enemyDistance = (enemy.transform.position - transform.position).sqrMagnitude;
+
+                if (enemyDistance < maxDistance)
+                {
+                    closestTarget = enemy.transform;
+                    maxDistance = enemyDistance;
+                }
+            }
+
+            mainTarget = closestTarget;
+            towerHead.LookAt(mainTarget);
+        }
+
+        private void ShootTarget()
+        {
+            animator.SetBool("IsShooting", mainTarget != null && animator.GetBool("IsOpen") && generator.IsOn);
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, attackRange);
+        }
+    }
+}
