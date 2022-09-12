d0b5ea563632a0b53ec964f5d4d9ad3059bc7231
Yee <yee.gamedev@gmail.com>
2022-09-12 16:16:26 -0500

    Energy Generator Script Created Controls Towers' Energy and Indicators

---
+++ Assets/Scripts/Tiles/EnergyGenerator.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace StarDef.Tiles
+{
+    public class EnergyGenerator : MonoBehaviour
+    {
+        [SerializeField] MeshRenderer indicator = null;
+        [SerializeField] Color damagedColor = Color.red;
+
+        List<MeshRenderer> paths = new List<MeshRenderer>();
+
+        Transform usedBy = null;
+
+        public void AddPath(List<MeshRenderer> path) { paths.AddRange(path); }
+
+        public bool IsOn { get => indicator.material.color != damagedColor; }
+        public bool CanBeUsed(Transform checkTransform)
+        {
+            return usedBy == null || usedBy == checkTransform;
+        }
+        public Transform UsedBy { set => usedBy = value; }
+
+        public void DamageGenerator()
+        {
+            indicator.material.color = damagedColor;
+
+            foreach (var indicator in paths)
+            {
+                indicator.material.color = Color.red;
+            }
+        }
+    }
+}
