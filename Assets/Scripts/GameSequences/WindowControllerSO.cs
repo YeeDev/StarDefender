97f33048d79576c10ca960cd41d935f8da1a3c36
Yee <yee.gamedev@gmail.com>
2022-09-20 13:25:39 -0500

    WindowController SO created

---
+++ Assets/Scripts/GameSequences/WindowControllerSO.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using UnityEngine;
+using StarDef.Info;
+
+namespace StarDef.GameSequences
+{
+    [CreateAssetMenu(fileName = "New Window Action", menuName = "Window Action")]
+    public class WindowControllerSO : ScriptableObject, ISequence
+    {
+        [SerializeField] bool opensWindow = true;
+        [SerializeField] float timeItTakes = 1f;
+
+        public IEnumerator PlaySequence(SequenceVariableHolder infoHolder)
+        {
+            if (opensWindow) { infoHolder.Printer.ClearDialogue(infoHolder.CommanderText); }
+
+            infoHolder.CommanderAnimator.SetBool("Open", opensWindow);
+
+            yield return new WaitForSeconds(timeItTakes);
+        }
+    }
+}
