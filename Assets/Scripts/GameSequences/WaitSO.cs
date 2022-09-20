6eab65d2411c6fe5fb837770e017991ca979b797
Yee <yee.gamedev@gmail.com>
2022-09-20 12:07:40 -0500

    WaitSO created

---
+++ Assets/Scripts/GameSequences/WaitSO.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using UnityEngine;
+
+namespace StarDef.GameSequences
+{
+    [CreateAssetMenu(fileName = "New Wait", menuName = "Wait")]
+    public class WaitSO : ScriptableObject, ISequence
+    {
+        [SerializeField][Range(0, 10)] float timeToWait = 0f;
+
+        public IEnumerator PlaySequence() { yield return new WaitForSeconds(timeToWait); }
+    }
+}
