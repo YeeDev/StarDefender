c324bdde34db25b1fd714ce92d3fbc88861a268f
Yee <yee.gamedev@gmail.com>
2022-09-20 13:24:44 -0500

    WaitInputSO script created

---
+++ Assets/Scripts/GameSequences/WaitInputSO.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using UnityEngine;
+using StarDef.Info;
+
+namespace StarDef.GameSequences
+{
+    [CreateAssetMenu(fileName = "New Wait Input", menuName = "Wait Input")]
+    public class WaitInputSO : ScriptableObject, ISequence
+    {
+        public IEnumerator PlaySequence(SequenceVariableHolder infoHolder)
+        {
+            return new WaitUntil(() => Input.anyKeyDown );
+        }
+    }
+}
