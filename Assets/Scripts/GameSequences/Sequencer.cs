402c42f85d4ccf8b160e49a1d1f9a3ebf4384ad9
Yee <yee.gamedev@gmail.com>
2022-09-20 13:24:31 -0500

    Sequencer Now Passes a InfoHolder

--- Assets/Scripts/GameSequences/Sequencer.cs
+++ Assets/Scripts/GameSequences/Sequencer.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using StarDef.Info;
+using UnityEngine.UI;
 
 namespace StarDef.GameSequences
 {
@@ -9,23 +11,22 @@ namespace StarDef.GameSequences
         [SerializeField] ScriptableObject[] gameSequence = null;
 
         HealthStat health;
+        SequenceVariableHolder infoHolder;
 
         private void Awake()
         {
             health = FindObjectOfType<HealthStat>();
+            infoHolder = new SequenceVariableHolder();
         }
 
-        private void Start()
-        {
-            StartCoroutine(RunWaves());
-        }
+        private void Start() { StartCoroutine(RunWaves()); }
 
         private IEnumerator RunWaves()
         {
             foreach (ISequence sequence in gameSequence)
             {
                 if(health.NoHealth) { yield break; }
-                yield return sequence.PlaySequence();
+                yield return sequence.PlaySequence(infoHolder);
             }
         }
     }
