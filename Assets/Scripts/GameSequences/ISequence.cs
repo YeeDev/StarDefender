c61d07da8abff17f60ce4ae2c3ea9adae59a0771
Yee <yee.gamedev@gmail.com>
2022-09-20 13:24:12 -0500

    ISequence now needs a SequenceVariableHolder

--- Assets/Scripts/GameSequences/ISequence.cs
+++ Assets/Scripts/GameSequences/ISequence.cs
@@ -1,9 +1,10 @@
 using System.Collections;
+using StarDef.Info;
 
 namespace StarDef.GameSequences
 {
     public interface ISequence
     {
-        public IEnumerator PlaySequence();
+        public IEnumerator PlaySequence(SequenceVariableHolder infoHolder);
     }
 }
