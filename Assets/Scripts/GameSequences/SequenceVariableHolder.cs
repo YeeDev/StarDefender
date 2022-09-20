58599950da3807de45ccd55c0747160180902f0a
Yee <yee.gamedev@gmail.com>
2022-09-20 13:22:47 -0500

    Sequence Variable Holder script created

---
+++ Assets/Scripts/GameSequences/SequenceVariableHolder.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Yee.Dialogue;
+
+namespace StarDef.Info
+{
+    public class SequenceVariableHolder
+    {
+        Text commanderText;
+        Animator commanderAnimator;
+        DialoguePrinter printer;
+
+        public Text CommanderText { get => commanderText; }
+        public Animator CommanderAnimator { get => commanderAnimator; }
+        public DialoguePrinter Printer { get => printer; }
+
+        List<object> debugList = new List<object>();
+
+        public SequenceVariableHolder()
+        {
+            printer = GameObject.FindObjectOfType<DialoguePrinter>();
+
+            GameObject commanderWindow = GameObject.FindGameObjectWithTag("CommanderWindow");
+            commanderAnimator = commanderWindow.GetComponent<Animator>();
+            commanderText = commanderWindow.GetComponentInChildren<Text>();
+
+            foreach (object item in debugList)
+            {
+                if(item == null) { Debug.Log(item + "equals null"); }
+            }
+        }
+    }
+}
