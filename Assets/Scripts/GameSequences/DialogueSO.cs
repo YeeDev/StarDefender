5baa1c6a796e8de869c665f59ffb915b161d5768
Yee <yee.gamedev@gmail.com>
2022-09-20 13:23:41 -0500

    DialogueSO script created

---
+++ Assets/Scripts/GameSequences/DialogueSO.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using StarDef.Info;
+using UnityEngine;
+using Yee.Dialogue;
+
+namespace StarDef.GameSequences
+{
+    [CreateAssetMenu(fileName = "New Dialogue", menuName = "Dialogue")]
+    public class DialogueSO : ScriptableObject, ISequence
+    {
+        [SerializeField] [TextArea(2, 5)] string dialogue = null;
+        [SerializeField] float waitBeforePrint = 1f;
+        [SerializeField] ScriptableObject beforePrintDecorator = null;
+        [SerializeField] ScriptableObject afterPrintDecorator = null;
+
+        public IEnumerator PlaySequence(SequenceVariableHolder infoHolder)
+        {
+            ISequence decoratorSequence;
+
+            if(beforePrintDecorator != null)
+            {
+                decoratorSequence = (ISequence)beforePrintDecorator;
+                yield return decoratorSequence.PlaySequence(infoHolder);
+            }
+
+            yield return infoHolder.Printer.PrintDialogue(dialogue, infoHolder.CommanderText);
+
+            if (afterPrintDecorator != null)
+            {
+                decoratorSequence = (ISequence)afterPrintDecorator;
+                yield return decoratorSequence.PlaySequence(infoHolder);
+            }
+        }
+    }
+}
