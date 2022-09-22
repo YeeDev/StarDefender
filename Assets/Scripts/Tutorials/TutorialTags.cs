5843a76d28feab01bc1577e60883fa23d0062762
Yee <yee.gamedev@gmail.com>
2022-09-22 11:05:05 -0500

    Tutorials Tags Enum created

---
+++ Assets/Scripts/Tutorials/TutorialTags.cs
@@ -0,0 +1,16 @@
+namespace StarDef.Tutorials
+{
+    public enum Tag
+    {
+        Tutorial_0,
+        Tutorial_1,
+        Tutorial_3,
+        Tutorial_4,
+        Tutorial_5,
+        Tutorial_6,
+        Tutorial_7,
+        Tutorial_8,
+        Tutorial_9,
+        Tutorial_Mask
+    }
+}
