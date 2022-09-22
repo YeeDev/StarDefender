a4a463c519169c3c8131219e35dbff7f46e47357
Yee <yee.gamedev@gmail.com>
2022-09-22 11:47:19 -0500

    TutorialSO script created

---
+++ Assets/Scripts/GameSequences/TutorialSO.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using StarDef.Info;
+using UnityEngine;
+using StarDef.Tutorials;
+
+namespace StarDef.GameSequences
+{
+    [CreateAssetMenu(fileName = "New Tutorial", menuName = "Tutorial")]
+    public class TutorialSO : ScriptableObject, ISequence
+    {
+        [SerializeField] Tag tutorialObject = Tag.Tutorial_0;
+
+        public IEnumerator PlaySequence(SequenceVariableHolder infoHolder)
+        {
+            Animator tutorialFader = infoHolder.TutorialMask.GetAnimator;
+            GameObject usedMask = infoHolder.GetTutorialObject(tutorialObject).gameObject;
+
+            tutorialFader.SetBool("InTutorial", true);
+            usedMask.SetActive(true);
+
+            yield return new WaitForSeconds(1f);
+
+            tutorialFader.SetBool("InTutorial", false);
+
+            yield return new WaitForSeconds(0.5f);
+
+            usedMask.SetActive(false);
+        }
+    }
+}
