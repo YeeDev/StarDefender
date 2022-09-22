5237fe79ed49b2f0aba797f39c101d7172762b79
Yee <yee.gamedev@gmail.com>
2022-09-22 11:47:07 -0500

    InfoHolder now searches for tutorial Masks

--- Assets/Scripts/GameSequences/SequenceVariableHolder.cs
+++ Assets/Scripts/GameSequences/SequenceVariableHolder.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Yee.Dialogue;
+using StarDef.Tutorials;
 
 namespace StarDef.Info
 {
@@ -14,8 +15,9 @@ namespace StarDef.Info
         public Text CommanderText { get => commanderText; }
         public Animator CommanderAnimator { get => commanderAnimator; }
         public DialoguePrinter Printer { get => printer; }
+        public TutorialTag TutorialMask { get => tutorialObjects[Tag.Tutorial_Mask]; }
 
-        List<object> debugList = new List<object>();
+        Dictionary<Tag, TutorialTag> tutorialObjects = new Dictionary<Tag, TutorialTag>();
 
         public SequenceVariableHolder()
         {
@@ -25,10 +27,36 @@ namespace StarDef.Info
             commanderAnimator = commanderWindow.GetComponent<Animator>();
             commanderText = commanderWindow.GetComponentInChildren<Text>();
 
-            foreach (object item in debugList)
+            CreateTutorialDictionary();
+        }
+
+        private void CreateTutorialDictionary()
+        {
+            foreach (TutorialTag tutorial in Resources.FindObjectsOfTypeAll<TutorialTag>())
             {
-                if(item == null) { Debug.Log(item + "equals null"); }
+                if (tutorialObjects.ContainsKey(tutorial.GetTag))
+                {
+                    string dupObjectName = tutorial.transform.name;
+                    string previousObjectName = tutorialObjects[tutorial.GetTag].transform.name;
+                    Debug.LogWarning($"Duplicated tutorial tag: {dupObjectName} same as {previousObjectName}");
+                    continue;
+                }
+
+                tutorialObjects.Add(tutorial.GetTag, tutorial);
             }
+
+            if (!tutorialObjects.ContainsKey(Tag.Tutorial_Mask))
+            {
+                Debug.LogWarning("No Tutorial Mask was found, tutorials won't work;");
+            }
+        }
+
+        public TutorialTag GetTutorialObject(Tag tutorialTag)
+        {
+            if (tutorialObjects.ContainsKey(tutorialTag)) { return tutorialObjects[tutorialTag]; }
+
+            Debug.LogError($"There's no tutorial object with tag {tutorialTag}.");
+            return null;
         }
     }
 }
