8b0c543e93318a349ad2ff70be9823229fb1d083
Yee <yee.gamedev@gmail.com>
2022-09-23 00:15:25 -0500

    Tutorial Waits for Interaction

--- Assets/Scripts/GameSequences/TutorialSO.cs
+++ Assets/Scripts/GameSequences/TutorialSO.cs
@@ -8,29 +8,29 @@ namespace StarDef.GameSequences
     [CreateAssetMenu(fileName = "New Tutorial", menuName = "Tutorial")]
     public class TutorialSO : ScriptableObject, ISequence
     {
-        [SerializeField] float waitTESTTime = 20f;
-        [SerializeField] MaskTag tutorialMask = MaskTag.Tutorial_0;
-        [SerializeField] bool stopsTime = false;
+        [SerializeField] MaskTag tutorialMask = MaskTag.NONE;
+        [SerializeField] ObjectTag tutorialObject = ObjectTag.NONE;
 
         public IEnumerator PlaySequence(SequenceVariableHolder infoHolder)
         {
-            if (stopsTime) { Time.timeScale = 0; }
-
             Animator tutorialFader = infoHolder.GetAnimatedMask.GetAnimator;
             GameObject usedMask = infoHolder.GetTutorialMask(tutorialMask).gameObject;
+            TutObjectTag tutObject = infoHolder.GetTutorialObject(tutorialObject);
 
             tutorialFader.SetBool("InTutorial", true);
             usedMask.SetActive(true);
 
-            yield return new WaitForSecondsRealtime(waitTESTTime);
+            if(tutObject != null)
+            {
+                tutObject.ChangeInTutorialStatus();
+                yield return new WaitUntil(() => tutObject.Pressed);
+            }
 
             tutorialFader.SetBool("InTutorial", false);
 
             yield return new WaitForSecondsRealtime(0.5f);
 
             usedMask.SetActive(false);
-
-            if (stopsTime) { Time.timeScale = 1; }
         }
     }
 }
