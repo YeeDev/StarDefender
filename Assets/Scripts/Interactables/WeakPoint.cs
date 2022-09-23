7704b9acd1a0dfec0ef8e6c1beb2dbb2239de8fd
Yee <yee.gamedev@gmail.com>
2022-09-23 17:40:00 -0500

    Goal Plays damage animation

--- Assets/Scripts/Interactables/WeakPoint.cs
+++ Assets/Scripts/Interactables/WeakPoint.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using StarDef.Core;
 using StarDef.Tiles;
@@ -8,14 +9,23 @@ namespace StarDef.Interactables
     {
         [SerializeField] ParticleSystem explosionParticles = null;
         [SerializeField] AudioSource audioSource = null;
-        [SerializeField] EnergyGenerator generatorAttached = null;
+        [SerializeField] EnergyGenerator[] generatorsAttached = null;
+        [SerializeField] MeshRenderer indicator = null;
+        [SerializeField] Color damagedColor = Color.red;
 
         bool weakPointDestroyed;
+        Color regularColor;
         HealthStat health;
+        Queue<EnergyGenerator> generatorsQueue = new Queue<EnergyGenerator>();
 
         public bool IsDestroyed { get => weakPointDestroyed; }
 
-        private void Awake() { health = FindObjectOfType<HealthStat>(); }
+        private void Awake()
+        {
+            health = FindObjectOfType<HealthStat>();
+            regularColor = indicator.material.color;
+            foreach (var item in generatorsAttached) { generatorsQueue.Enqueue(item); }
+        }
 
         private void OnTriggerEnter(Collider other)
         {
@@ -23,10 +33,28 @@ namespace StarDef.Interactables
             {
                 weakPointDestroyed = true;
                 Destroy(other.gameObject);
-                audioSource.Play();
-                explosionParticles.Play();
-                health.TakeDamage(generatorAttached);
+
+                TakeDamage();
+                PlayVFX();
             }
+        }
+
+        private void PlayVFX()
+        {
+            audioSource.Play();
+            explosionParticles.Play();
+            ChangeColorToDamage();
+
+            if (generatorsQueue.Count > 0) { Invoke("ChangeColorToRegular", 1); }
+        }
+
+        private void ChangeColorToDamage() { indicator.material.color = damagedColor; }
+        private void ChangeColorToRegular() { indicator.material.color = regularColor; }
+
+        private void TakeDamage()
+        {
+            EnergyGenerator generatorToDestroy = generatorsQueue.Count > 0 ? generatorsQueue.Dequeue() : generatorsAttached[0];
+            health.TakeDamage(generatorToDestroy);
         }
     }
 }
