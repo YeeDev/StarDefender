846abb5e77d7a665a049bb07630051e63daa6365
Yee <yee.gamedev@gmail.com>
2022-09-23 18:10:06 -0500

    CameraShaker Script Imported

---
+++ Assets/Scripts/Yee/CameraShaker.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using UnityEngine;
+
+namespace Yee.VFX
+{
+	public class CameraShaker : MonoBehaviour
+	{
+		[SerializeField] [Range(1, 10)] float shakeDuration = 1f;
+		[SerializeField] [Range(0.1f, 5f)] float shakeIntensity = 1f;
+
+		Vector3 originalPos;
+
+		private void Awake() { originalPos = transform.position; }
+
+		public IEnumerator Shake()
+		{
+			float shakeTimer = shakeDuration;
+			while (shakeTimer > 0)
+			{
+				shakeTimer -= Time.deltaTime;
+				transform.position = originalPos + Random.insideUnitSphere * shakeIntensity;
+
+				yield return new WaitForEndOfFrame();
+			}
+
+			transform.position = originalPos;
+		}
+	}
+}
