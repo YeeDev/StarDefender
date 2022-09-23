0734f2e285048de40370caa806e68832e3f41cb0
Yee <yee.gamedev@gmail.com>
2022-09-23 12:24:39 -0500

    Change GameStarter name to ControlEnabler

---
+++ Assets/Scripts/Core/ControlEnabler.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+namespace StarDef.Core
+{
+    public class ControlEnabler : MonoBehaviour
+    {
+        public Action<bool> OnControlChange;
+
+        public void StartGame(bool isControlEnabled) { if (OnControlChange != null) { OnControlChange(isControlEnabled); } }
+    }
+}
