0aba31e4de0ef3807cf40bdf6a103c482ab5724c
Yee <yee.gamedev@gmail.com>
2022-09-23 12:04:05 -0500

    GameStarted script created

---
+++ Assets/Scripts/Core/GameStarter.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+namespace StarDef.Core
+{
+    public class GameStarter : MonoBehaviour
+    {
+        public Action OnGameStart;
+
+        public void StartGame() { if (OnGameStart != null) { OnGameStart(); } }
+    }
+}
