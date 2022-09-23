ea3f51c2dd8a63709acd4684a2fb9e59ea71803e
Yee <yee.gamedev@gmail.com>
2022-09-23 12:24:56 -0500

    Created ControlEnablerSO script

---
+++ Assets/Scripts/GameSequences/ControlEnablerSO.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using UnityEngine;
+using StarDef.Info;
+
+namespace StarDef.GameSequences
+{
+    [CreateAssetMenu(fileName = "New Control Enabler", menuName = "Control Enabler")]
+    public class ControlEnablerSO : ScriptableObject, ISequence
+    {
+        [SerializeField] bool enableControl = false;
+
+        public IEnumerator PlaySequence(SequenceVariableHolder infoHolder)
+        {
+            infoHolder.GameStarter.OnControlChange(enableControl);
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
