194f8ba1b3d8e90d6c8212fc52ff393532a713b9
Yee <yee.gamedev@gmail.com>
2022-09-22 19:46:07 -0500

    Change Tutorial Tag to TutMaskTag

---
+++ Assets/Scripts/Tutorials/TutMaskTag.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace StarDef.Tutorials
+{
+    public class TutMaskTag : MonoBehaviour
+    {
+        [SerializeField] MaskTag tutorialTag = MaskTag.Tutorial_0;
+        Animator animator;
+
+        public MaskTag GetTag { get => tutorialTag; }
+        public Animator GetAnimator { get => animator; }
+
+        private void Awake() { if (tutorialTag == MaskTag.Tutorial_Mask) { animator = GetComponent<Animator>(); } }
+    }
+}
