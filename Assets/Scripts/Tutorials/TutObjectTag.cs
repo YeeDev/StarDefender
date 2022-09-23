0cf816ccbef1d54a97b090e4ce33ee9f8a10019f
Yee <yee.gamedev@gmail.com>
2022-09-23 00:14:08 -0500

    Tutorial Object only checks interactions when in Tutorial

--- Assets/Scripts/Tutorials/TutObjectTag.cs
+++ Assets/Scripts/Tutorials/TutObjectTag.cs
@@ -4,13 +4,16 @@ namespace StarDef.Tutorials
 {
     public class TutObjectTag : MonoBehaviour
     {
-        [SerializeField] ObjectTag tutorialTag = ObjectTag.TutObject_0;
+        [SerializeField] ObjectTag tutorialTag = ObjectTag.NONE;
 
         bool pressed;
+        bool inTutorial;
 
+        public bool InTutorial { get => inTutorial; }
         public ObjectTag GetTag { get => tutorialTag; }
         public bool Pressed { get => pressed; }
 
         public void InteractWithObject() { pressed = true; }
+        public void ChangeInTutorialStatus() { inTutorial = !inTutorial; }
     }
 }
