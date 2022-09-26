f087a5d3bc5daf72ec0eae6e013cba3c98835352
Yee <yee.gamedev@gmail.com>
2022-09-26 11:18:00 -0500

    EndMessageSO script created

---
+++ Assets/Scripts/GameSequences/EndMessageSO.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using StarDef.Info;
+using UnityEngine;
+
+namespace StarDef.GameSequences
+{
+    [CreateAssetMenu(fileName = "New End Message", menuName = "End Message")]
+    public class EndMessageSO : ScriptableObject, ISequence
+    {
+        [SerializeField] bool showWinMessage = true;
+
+        public IEnumerator PlaySequence(SequenceVariableHolder infoHolder)
+        {
+            string messageToShow = showWinMessage ? "Win" : "Lose";
+            infoHolder.MessagesAnimator.SetTrigger(messageToShow);
+
+            yield return null;
+        }
+    }
+}
