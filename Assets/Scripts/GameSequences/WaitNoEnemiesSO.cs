51d2392a5fc5fb8132bdaeb8d99793261d0237c8
Yee <yee.gamedev@gmail.com>
2022-09-26 11:38:56 -0500

    WaitNoEnemiesSO script created

---
+++ Assets/Scripts/GameSequences/WaitNoEnemiesSO.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using StarDef.Info;
+using UnityEngine;
+
+namespace StarDef.GameSequences
+{
+    [CreateAssetMenu(fileName = "New Wait Enemies", menuName = "Wait No Enemies")]
+    public class WaitNoEnemiesSO : MonoBehaviour, ISequence
+    {
+        public IEnumerator PlaySequence(SequenceVariableHolder infoHolder)
+        {
+            yield return new WaitUntil(() => infoHolder.GetActiveEnemies <= 0);
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
