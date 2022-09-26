050f723696e93982dabe4db7cb4133ec37f85a39
Yee <yee.gamedev@gmail.com>
2022-09-26 12:17:33 -0500

    LoaderSO script created

---
+++ Assets/Scripts/GameSequences/LoaderSO.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using StarDef.Info;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace StarDef.GameSequences
+{
+    [CreateAssetMenu(fileName = "New Loader", menuName = "Static Sequences/Loader")]
+    public class LoaderSO : ScriptableObject, ISequence
+    {
+        [SerializeField] bool loadNextLevel = true;
+
+        public IEnumerator PlaySequence(SequenceVariableHolder infoHolder)
+        {
+            infoHolder.FaderAnimator.SetTrigger("Fade");
+
+            yield return new WaitForSeconds(1f);
+
+            int currentScene = SceneManager.GetActiveScene().buildIndex;
+            int sceneToLoad = loadNextLevel ? (currentScene + 1) % SceneManager.sceneCountInBuildSettings : currentScene;
+
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
