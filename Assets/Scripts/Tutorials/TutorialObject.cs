f6c75e25147e7294b1cdbee534bed8a24bcc6fde
Yee <yee.gamedev@gmail.com>
2022-09-27 09:56:13 -0500

    TutorialObject can now be reseted and reused.

--- Assets/Scripts/Tutorials/TutorialObject.cs
+++ Assets/Scripts/Tutorials/TutorialObject.cs
@@ -7,13 +7,18 @@ namespace StarDef.Tutorials
         [SerializeField] ObjectTag tutorialTag = ObjectTag.NONE;
 
         bool pressed;
-        bool inTutorial;
+        [SerializeField]bool inTutorial;
 
         public bool InTutorial { get => inTutorial; }
         public ObjectTag GetTag { get => tutorialTag; }
         public bool Pressed { get => pressed; }
 
         public void InteractWithObject() { pressed = true; }
-        public void ChangeInTutorialStatus() { inTutorial = !inTutorial; }
+        public void TurnTurotialOn() { inTutorial = true; }
+        public void ResetObject()
+        {
+            inTutorial = false;
+            pressed = false;
+        }
     }
 }
