cad6d580be064e82029cef36a3c31cd94c5c9095
Yee <yee.gamedev@gmail.com>
2022-09-30 12:57:35 -0500

    Created Singleton Script

---
+++ Assets/Scripts/Core/Singleton.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace StarDef.Core
+{
+    public class Singleton : MonoBehaviour
+    {
+        bool isSingleton;
+
+        public bool IsSingleton { get => isSingleton; }
+
+        private void Awake()
+        {
+            foreach (Singleton singleton in FindObjectsOfType<Singleton>())
+            {
+                if (singleton.isSingleton && singleton != this)
+                {
+                    Destroy(gameObject);
+                }
+            }
+
+            DontDestroyOnLoad(this);
+            isSingleton = true;
+        }
+    }
+}
