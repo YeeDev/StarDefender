119b084b4c5581398e077f45a531cbd9e4f0db7f
Yee <yee.gamedev@gmail.com>
2022-09-30 10:25:15 -0500

    Health script now controls light animation

--- Assets/Scripts/Core/HealthStat.cs
+++ Assets/Scripts/Core/HealthStat.cs
@@ -5,11 +5,19 @@ namespace StarDef.Core
     public class HealthStat : MonoBehaviour
     {
         int totalHealth;
+        Animator animator;
+
+        private void Awake() { animator = GetComponent<Animator>(); }
 
         public bool NoHealth { get => totalHealth <= 0; }
 
-        public void AddHealth(int amountToAdd) { totalHealth += amountToAdd; }
+        public void AddHealth(int amountToAdd) { totalHealth += amountToAdd;Debug.Log(totalHealth); }
 
-        public void TakeDamage() { totalHealth--; }
+        public void TakeDamage(bool lowerHealth)
+        {
+            if (lowerHealth) { totalHealth--; }
+            animator.SetTrigger("Hit");
+            animator.SetInteger("Health", totalHealth);
+        }
     }
 }
